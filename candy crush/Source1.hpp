e86f3a82cc807b7ed6d7216ffcaf9ea381057f01
hafsa-zia <137703845+hafsa-zia@users.noreply.github.com>
2023-07-31 21:06:42 +0500

    Add files via upload

---
+++ candy crush/Source1.hpp
@@ -0,0 +1,15 @@
+#pragma once
+# ifndef Header
+# define Header 
+int board1(char array[8][8]);
+void coloring(char array[8][8]);
+void coloring1(char array[10][10]);
+void newCandy();
+void newCandy2();
+void candy_crusher1(char array[8][8], int& points);
+void inputs(int& row, int& column, int& gameMoves);
+void board2(char array2[10][10]);
+void candy_crusher2(char array2[10][10], int& points);
+void value_assign1(char array[8][8]);
+void value_assign2(char array2[10][10]);
+#endif
